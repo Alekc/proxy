e1d420c34256bd80e2bd0d85e39ca12761cca46f
Alexander Chernov <alekcander@gmail.com>
2018-06-02 22:30:34 +0100

    Added constructor from simple string to proxy object.

--- proxy.go
+++ proxy.go
@@ -1,6 +1,13 @@
 package proxy
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var proxyRegex = regexp.MustCompile(`((?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])):(\d{1,5})`)
 
 //easyjson:json
 type Proxy struct {
@@ -9,6 +16,30 @@ type Proxy struct {
 	Type int
 }
 
-func (px *Proxy) ToString() string {
-	return fmt.Sprintf("%s:%s", px.Ip, px.Port)
+func (v *Proxy) ToString() string {
+	return fmt.Sprintf("%s:%d", v.Ip, v.Port)
+}
+
+// Converts ipv4 string (xxx.xx.xxx.xxx:xxxxx) to Proxy object
+func FromIpv4String(proxyString string) (*Proxy, error) {
+	if proxyString == "" {
+		return nil, errors.New("input string is empty")
+	}
+
+	//extract ip and port
+	matchResult := proxyRegex.FindAllStringSubmatch(proxyString, -1)
+	if len(matchResult) != 1 && len(matchResult[0]) != 3 {
+		return nil, errors.New("this is not a valid ipv4 address")
+	}
+
+	//convert port to appropriate type
+	port, err := strconv.Atoi(matchResult[0][2])
+	if err != nil {
+		return nil, err
+	}
+	px := &Proxy{
+		Ip:   matchResult[0][1],
+		Port: port,
+	}
+	return px, nil
 }
